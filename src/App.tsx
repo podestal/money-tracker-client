24e413c7199b61ee8a0aee2a70a8e927cc59fe0a
podestal <luisrodriguezpodesta@gmail.com>
2024-09-01 08:30:03 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+
+
+const App = () => {
+
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <>
+      <h2 className="text-4xl text-red-500">Hola</h2>
+    </>
+  )
+}
+
+export default App
