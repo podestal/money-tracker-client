0781d8fcb95b047f5ffa5f2ffb34ef0d3a4fbc54
podestal <luisrodriguezpodesta@gmail.com>
2024-10-05 12:39:52 -0400

    create project popup

---
+++ src/components/projects/CreateProject.tsx
@@ -0,0 +1,42 @@
+import { RiAddCircleFill } from "@remixicon/react"
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogTitle, DialogTrigger, DialogHeader, DialogFooter } from "../ui/Dialog";
+import { Button } from "../ui/Button"
+import ProjectForm from "./ProjectForm"
+
+const CreateProject = () => {
+  return (
+    <Dialog>
+        {/* Button to trigger the dialog */}
+        <DialogTrigger asChild>
+            <div>
+                <RiAddCircleFill 
+                    className="cursor-pointer"
+                    color="blue"
+                    size={40}
+                />
+            </div>
+        </DialogTrigger>
+
+        {/* Dialog content for creating a new transaction */}
+        <DialogContent>
+        <DialogHeader>
+            <DialogTitle className="text-3xl text-center my-6">New Project</DialogTitle>
+        </DialogHeader>
+        <DialogDescription asChild>
+            <ProjectForm />
+            {/* Form component to create a new transaction */}
+            {/* <TransactionForm createTransaction={createTransaction} access={access} /> */}
+        </DialogDescription>
+        <DialogFooter>
+            <DialogClose asChild>
+            <div className="w-full flex justify-center items-center">
+                <Button variant="destructive">Close</Button>
+            </div>
+            </DialogClose>
+        </DialogFooter>
+        </DialogContent>
+    </Dialog>
+  )
+}
+
+export default CreateProject
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,6 +1,7 @@
 import useGetProjects from "../../hooks/api/projects/useGetProjects"
 import useAuthStore from "../../hooks/store/useAuthStore"
 import ProjectCard from "./ProjectCard"
+import CreateProject from "./CreateProject"
 
 const Projects = () => {
 
@@ -15,11 +16,14 @@ const Projects = () => {
 
   return (
     <div className="flex flex-col justify-center items-center py-10 gap-12">
-      <h2 className="text-6xl">Projects</h2>
+      <div className="flex justify-center items-center gap-10">
+        <h2 className="text-6xl">Projects</h2>
+        <CreateProject />
+      </div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
-        
         {projects.map(project => (
             <ProjectCard 
+                key={project.id}
                 project={project}
             />
         ))}
