0f1e2d8a5b799a5fc1a08383f9624e74dbe0b52b
podestal <luisrodriguezpodesta@gmail.com>
2024-10-06 10:57:42 -0400

    project form working

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -13,10 +13,10 @@ const ProjectCard = ({ project }: Props) => {
         state={project}
         className="flex flex-col gap-4 bg-slate-900 pt-8 px-4 rounded-2xl hover:bg-slate-800"
     >
-        <h2 className="text-2xl text-center font-bold">{project.name}</h2>
+        <h2 className="text-3xl text-blue-500 text-center font-bold mb-6">{project.name}</h2>
         <div className="grid grid-rows-2 h-full gap-6">
             <p className="text-sm text-slate-300">{project.description}</p>
-            {project.end_date && <p>Due on: {moment(project?.end_date).format('YYYY/MM/DD')}</p>}
+            <p>Due on: {project.end_date &&  moment(project?.end_date).format('YYYY/MM/DD')}</p>
         </div>
     </Link>
   )
--- src/components/projects/ProjectForm.tsx
+++ src/components/projects/ProjectForm.tsx
@@ -1,29 +1,55 @@
-import { useState } from "react";
+import { useRef, useState } from "react"
 import { Input } from "../ui/InputText"
-import DatePicker from 'react-datepicker'
+import { Button } from "../ui/Button"
 
 const ProjectForm = () => {
 
-    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+    const nameRef = useRef<HTMLInputElement>(null)
+    const descriptionRef = useRef<HTMLTextAreaElement>(null)
+
+    const [success, setSuccess] = useState('')
+    const [error, setError] = useState('')
+
+    const handleProjectForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setSuccess('')
+        setError('')
+
+        const name = nameRef.current?.value
+        const description = descriptionRef.current?.value
+        
+        if (!name) {
+            setError('Name is required')
+            return
+        }
+
+        if (!description) {
+            setError('Description is required')
+            return
+        }
+
+        setSuccess('Project created')
+
+    }
+
+
   return (
     <form
+        onSubmit={handleProjectForm}
         className="flex flex-col justify-center items-center gap-6 w-[70%] mx-auto my-6"
     >
+        {success && <p className="text-green-500">{success}</p>}
+        {error && <p className="text-red-500">{error}</p>}
         <Input 
             placeholder="Project name ..."
+            ref={nameRef}
         />
-        <textarea  // Textarea for transaction description
+        <textarea 
             placeholder="Description"
-            // ref={descriptionRef}
+            ref={descriptionRef}
             className="bg-gray-950 border-gray-800 rounded-lg w-full text-sm text-slate-50 h-[100px]"
         />
-        <DatePicker
-            selected={selectedDate}
-            onChange={(date: Date | null) => setSelectedDate(date)}
-            dateFormat="dd/MM/yyyy"  // Customize date format
-            isClearable  // Add a clear button
-            placeholderText="Select a date"  // Placeholder text
-        />
+        <Button disabled={success ? true : false}>Add Project</Button>
     </form>
   )
 }
