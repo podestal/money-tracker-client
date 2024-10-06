a6dcc495fd6e008ab88030d4552e5cfeabb997d6
podestal <luisrodriguezpodesta@gmail.com>
2024-10-06 11:27:56 -0400

    styled single project

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -8,9 +8,11 @@ const ProjectPage = () => {
   const project:Project = location.state
 
   return (
-    <div className="text-slate-50 min-h-screen xl:max-w-[1060px] mx-auto">
-      <h2>{project.name}</h2>
-      <p>{project.description}</p>
+    <div className="text-slate-50 min-h-screen xl:max-w-[1060px] mx-auto py-10">
+      <div className="flex flex-col gap-6">
+        <h2 className="text-4xl text-blue-500 font-bold">{project.name}</h2>
+        <p className="text-slate-400">{project.description}</p>
+      </div>
       <Tasks 
         projectId={project.id}
       />
