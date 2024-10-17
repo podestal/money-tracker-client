16b39b8ce25867d456e317b341158c3ca2e545cc
podestal <luisrodriguezpodesta@gmail.com>
2024-10-17 13:19:41 -0400

    update task hook implemented

---
+++ src/hooks/api/tasks/useUpdateTask.ts
@@ -0,0 +1,29 @@
+import { UseMutationResult, useMutation, useQueryClient } from "@tanstack/react-query"
+import getTaskService, { Task, TaskCreateUpdate } from "../../../services/api/tasksService"
+import { getTasksCacheKey } from "../../../lib/constants"
+
+interface UpdateTaskData {
+    access: string
+    updates: TaskCreateUpdate
+}
+
+interface Props {
+    taskId: number
+    projectId: number
+}
+
+const useUpdateTask = ({ taskId, projectId }: Props): UseMutationResult<Task, Error, UpdateTaskData> => {
+    const taskService = getTaskService({projectId, taskId})
+    const queryClient = useQueryClient()
+    const TASK_CACHE_KEY = getTasksCacheKey(projectId)
+    return useMutation({
+        mutationFn: (data: UpdateTaskData) => taskService.update(data.updates, data.access),
+        onSuccess: res => {
+            console.log(res)
+            queryClient.invalidateQueries({queryKey: TASK_CACHE_KEY})
+        },
+        onError: err => console.log(err)
+    })
+}
+
+export default useUpdateTask
