36912bd8146b250a525bde8ceeddf820d33e672b
podestal <luisrodriguezpodesta@gmail.com>
2024-10-24 13:31:35 -0400

    remove project hook implemented

---
+++ src/hooks/api/projects/useRemoveProject.ts
@@ -0,0 +1,20 @@
+import { UseMutationResult, useMutation, useQueryClient } from "@tanstack/react-query"
+import getProjectService, { Project } from "../../../services/api/projectsService"
+import { getProjectCacheKey } from "../../../lib/constants"
+
+interface DeleteProjectData {
+    access: string
+}
+
+const useRemoveProject = (projectId: number): UseMutationResult<Project, Error, DeleteProjectData> => {
+    const projectService = getProjectService({projectId})
+    const PROJECT_CACHE_KEY = getProjectCacheKey(true, projectId)
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: (data: DeleteProjectData) => projectService.delete(data.access),
+        onSuccess: () => queryClient.invalidateQueries({queryKey: PROJECT_CACHE_KEY}),
+        onError: err => console.log(err),
+    })
+}
+
+export default useRemoveProject
