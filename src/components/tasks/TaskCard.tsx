ecedea2ecd97a456c1a857b091cd1f4954ff92b6
podestal <luisrodriguezpodesta@gmail.com>
2024-10-26 11:52:21 -0400

    task card ui for grab and add user

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,6 +1,7 @@
 import useTaskTransferStore from "../../hooks/store/useTaskTransferStore"
 import { Task } from "../../services/api/tasksService"
 import { motion } from 'framer-motion'
+import { RiDragMove2Fill, RiUserAddFill } from "@remixicon/react"
 
 interface Props {
     task: Task
@@ -19,8 +20,14 @@ const TaskCard = ({ task }: Props) => {
         layout
         draggable 
         onDragStart={handleDragStart}
-        className="cursor-grab rounded border border-slate-800 bg-slate-900 p-3 active:cursor-grabbing my-2 text-xs">
-        <p>{task.name}</p>
+        className="rounded border border-slate-800 bg-slate-900 p-3 active:cursor-grabbing my-2 text-xs">
+          <div className="flex justify-between items-start gap-2 overflow-scroll">
+            <p>{task.name}</p>
+            <div className="flex justify-center items-center gap-2">
+              <RiUserAddFill size={18} className="text-green-600 hover:text-green-700 hover:cursor-pointer"/>
+              <RiDragMove2Fill size={18} className="text-blue-600  hover:cursor-grab"/>
+            </div>
+          </div>
       </motion.div>
   )
 }
