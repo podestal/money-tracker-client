7c8d11f146acc523f1ac1aa1a61220369844ddf8
podestal <luisrodriguezpodesta@gmail.com>
2024-10-30 20:28:14 -0400

    team service

---
+++ src/services/api/teamService.ts
@@ -0,0 +1,18 @@
+import APIClient from "./apiClient"
+
+export interface Team {
+    id: number
+    user: number
+    members: number[]
+}
+
+interface Props {
+    teamId: number
+}
+
+const getTeamService = ({ teamId }: Props) => {
+    const URL = teamId ? `teams/${teamId}` : 'teams/'
+    return new APIClient<Team>(URL)
+}
+
+export default getTeamService
