9ae03835456ed6dcbc854cc3c510773afa3382a2
podestal <luisrodriguezpodesta@gmail.com>
2024-11-04 11:40:15 -0500

    transaction update modal when click in transaction card

--- src/components/transactions/TransactionCard.tsx
+++ src/components/transactions/TransactionCard.tsx
@@ -3,6 +3,7 @@ import RemoveTransaction from "./RemoveTransaction" // Importing the RemoveTrans
 import UpdateTransaction from "./UpdateTransaction"
 import moment from "moment"
 import { motion } from 'framer-motion'
+import { useState } from "react"
 
 // Props interface for the TransactionCard component
 interface Props {
@@ -12,11 +13,17 @@ interface Props {
 // Component for displaying a transaction's details
 const TransactionCard = ({ transaction }: Props) => {
 
+    const [open, setOpen] = useState(false)
+
+    const handleClose = () => {
+        setOpen(false)
+    }
   // Rendering the transaction details
   return (
     <motion.div 
         layout
-        className={`overflow-scroll grid grid-cols-5 lg:grid-cols-7 gap-20 bg-slate-900 hover:bg-slate-800 w-full py-2 my-2 px-4 rounded-3xl`}>
+        onClick={() => setOpen(true)}
+        className={`overflow-scroll grid grid-cols-4 lg:grid-cols-6 gap-20 bg-slate-900 hover:bg-slate-800 w-full py-2 my-2 px-4 rounded-3xl`}>
         <p className={`text-sm font-bold text-right my-auto ${transaction.transaction_type === 'IN' ? 'text-green-500' : 'text-red-500'}`}>
             {(transaction.amount).toFixed(2)}
         </p> {/* Display the amount of transaction with conditional styling */}
@@ -29,9 +36,11 @@ const TransactionCard = ({ transaction }: Props) => {
         <p className="text-xs lg:col-span-2 my-auto hidden lg:block">
             {moment(transaction.created_at).format("dddd MMM Do")}
         </p>
-        <div className="w-full flex items-center justify-center lg:justify-between gap-4 lg:col-span-2">
+        <div className="w-full flex items-center justify-center lg:justify-between gap-4">
             <UpdateTransaction 
                 transaction={transaction} // Pass the transaction object to the UpdateTransaction component
+                open={open}
+                handleClose={handleClose}
             />
             <RemoveTransaction 
                 transaction={transaction} // Pass the transaction object to the RemoveTransaction component
--- src/components/transactions/UpdateTransaction.tsx
+++ src/components/transactions/UpdateTransaction.tsx
@@ -3,51 +3,66 @@ import useUpdateTransaction from "../../hooks/api/transactions/useUpdateTransact
 import useAuthStore from "../../hooks/store/useAuthStore"
 import { Transaction } from "../../services/api/transactionsService"
 import { Button } from "../ui/Button";
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogTitle, DialogTrigger, DialogHeader, DialogFooter } from "../ui/Dialog";
 import TransactionForm from "./TransactionForm";
+import { useState } from "react";
+import Modal from "../ui/Modal";
 
 interface Props {
     transaction: Transaction
+    open: boolean
+    handleClose: () => void
 }
 
-const UpdateTransaction = ({ transaction }: Props) => {
+const UpdateTransaction = ({ transaction, open, handleClose }: Props) => {
 
     const updateTransaction = useUpdateTransaction(transaction.id)
     const access = useAuthStore(s => s.access) || ''
 
   return (
-    <Dialog>
-        {/* Button to trigger the dialog */}
-        <DialogTrigger asChild>
-            <div>
-                <RiPencilFill 
-                    className="text-blue-500 cursor-pointer hover:text-blue-600"
-                    size={18}
-                />
-            </div>
-        </DialogTrigger>
+    <Modal
+        isOpen={open}
+        onClose={handleClose}
+        title="Update transaction"
+    >
+        <TransactionForm 
+            updateTransaction={updateTransaction} 
+            access={access} 
+            transaction={transaction}/>
+    </Modal>
 
-        {/* Dialog content for creating a new transaction */}
-        <DialogContent>
-        <DialogHeader>
-            <DialogTitle>New Transaction</DialogTitle>
-        </DialogHeader>
-        <DialogDescription asChild>
-            {/* Form component to create a new transaction */}
-            <TransactionForm 
-                updateTransaction={updateTransaction} 
-                access={access} 
-                transaction={transaction}/>
-        </DialogDescription>
-        <DialogFooter>
-            <DialogClose asChild>
-                <div className="w-full flex justify-center items-center">
-                    <Button variant="destructive">Close</Button>
-                </div>
-            </DialogClose>
-        </DialogFooter>
-        </DialogContent>
-    </Dialog>
+
+    // <Dialog>
+    //     {/* Button to trigger the dialog */}
+    //     <DialogTrigger asChild>
+    //         <div>
+                // <RiPencilFill 
+                //     className="text-blue-500 cursor-pointer hover:text-blue-600"
+                //     size={18}
+                // />
+    //         </div>
+    //     </DialogTrigger>
+
+    //     {/* Dialog content for creating a new transaction */}
+    //     <DialogContent>
+    //     <DialogHeader>
+    //         <DialogTitle>New Transaction</DialogTitle>
+    //     </DialogHeader>
+    //     <DialogDescription asChild>
+    //         {/* Form component to create a new transaction */}
+            // <TransactionForm 
+            //     updateTransaction={updateTransaction} 
+            //     access={access} 
+            //     transaction={transaction}/>
+    //     </DialogDescription>
+    //     <DialogFooter>
+    //         <DialogClose asChild>
+    //             <div className="w-full flex justify-center items-center">
+    //                 <Button variant="destructive">Close</Button>
+    //             </div>
+    //         </DialogClose>
+    //     </DialogFooter>
+    //     </DialogContent>
+    // </Dialog>
   )
 }
 
